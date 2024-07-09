fc9a48c8b0d2a852d9344f78ee259d3d5ef33eb9
Rie Amasato <rie_amasato@icloud.com>
2024-07-09 06:39:16 +0000

    Gridレイアウトの作成

---
+++ components/amasugar/Gridlayout.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="grid container white">
+    <div class="s1 e5">
+        <nl>
+            <li>
+                <code>clas="grid"</code>を使用することでその要素が縦に4分割されます。
+            </li>
+            <li>
+                CSSの Gridよろしく、最左端が1、最右端が5となります
+            </li>
+            <li>
+                開始番号を設定するには、<code>class="s1"</code>のように<code>s${number}</code>の形で指定します
+            </li>
+            <li>
+                終了番号も同じように、<code>class="e5"</code>のように<code>e${number}</code>の形で指定します
+            </li>
+        </nl>
+    </div>
+    <div class="container green s1 e3">
+        <code>
+        class="container green s1 e3"
+        </code>
+    </div>
+    <div class="container blue s2 e5">
+        <code>
+        class="container green s2 e5"
+        </code>
+    </div>
+</div>
+</template>
---
+++ pages/ils-food-cake/gridlayout.vue
@@ -0,0 +1,19 @@
+<template> 
+<div class="flex-8">
+    <div class="w-100">
+        <amasugar-gridlayout />
+    </div>
+</div>
+</template>
+
+<script setup>
+    useHead({title: "あま砂糖"})
+
+</script>
+
+<style scoped>
+
+.w-80p{
+    width: 70%;
+}
+</style>
