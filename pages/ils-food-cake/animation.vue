3a0293dffe6c607bb9ec3f464384cce41d2dbdc3
Rie Amasato <rie_amasato@icloud.com>
2024-10-02 08:38:30 +0000

    アニメーション用仮ページの設置

---
+++ pages/ils-food-cake/animation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container white">
+        <p>ローディング用伸びる(?)素材</p>
+        <p>ちなみに、伸びるアイスは"ドンドゥルマ"いうらしいですよ</p>
+        <div style="display: flex;gap: 16px">
+            <div class="dondurma white" style="animation-delay: -0.8s" />
+            <div class="dondurma violet" style="animation-delay: -0.7s" />
+            <div class="dondurma pink" style="animation-delay: -0.6s" />
+            <div class="dondurma red" style="animation-delay: -0.5s" />
+            <div class="dondurma orange" style="animation-delay: -0.4s" />
+            <div class="dondurma yellow" style="animation-delay: -0.3s" />
+            <div class="dondurma green" style="animation-delay: -0.2s" />
+            <div class="dondurma blue" style="animation-delay: -0.1s" />
+        </div>
+    </div>
+
+    <div class="container white">
+        まわるドーナツ
+        <div class="rotate" style="width: fit-content;">
+        🍩
+        </div>
+    </div>
+</template>
