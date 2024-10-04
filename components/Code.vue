a41029dc94b3216da26c9ce168e0ddf7d28c066c
Rie Amasato <rie_amasato@icloud.com>
2024-10-04 02:23:36 +0000

    ドンドゥルマを追加しました

---
+++ components/Code.vue
@@ -0,0 +1,15 @@
+<template>
+    <code class="codebox">
+        {{code}}
+    </code>
+</template>
+
+<style scoped>
+.codebox{
+    white-space: pre-wrap;
+}
+</style>
+
+<script setup>
+const props=defineProps(["code"])
+</script>
--- pages/ils-food-cake/animation.vue
+++ pages/ils-food-cake/animation.vue
@@ -1,23 +1,57 @@
 <template>
     <div class="container white">
-        <p>ローディング用伸びる(?)素材</p>
-        <p>ちなみに、伸びるアイスは"ドンドゥルマ"いうらしいですよ</p>
-        <div style="display: flex;gap: 16px">
-            <div class="dondurma white" style="animation-delay: -0.8s" />
-            <div class="dondurma violet" style="animation-delay: -0.7s" />
-            <div class="dondurma pink" style="animation-delay: -0.6s" />
-            <div class="dondurma red" style="animation-delay: -0.5s" />
-            <div class="dondurma orange" style="animation-delay: -0.4s" />
-            <div class="dondurma yellow" style="animation-delay: -0.3s" />
-            <div class="dondurma green" style="animation-delay: -0.2s" />
-            <div class="dondurma blue" style="animation-delay: -0.1s" />
+        <h2>ローディング用伸びる(?)素材</h2>
+        <p>ちなみに、トルコの伸びるアイスは"ドンドゥルマ"言って、原材料に秘密があるらしいですよ</p>
+        <p>並べるとこんな感じ</p>
+        <div class="mh-auto"
+            style="
+                display: flex;
+                gap: 16px"
+            >
+                <div v-for="(color, i) in colors.slice(1)" 
+                  :class="{dondurma: true, [color]: true}" 
+                  :style="`animation-delay: -${Math.floor((i+1)*2, 3)/10}s`" 
+                />
+        </div>
+
+        <div v-for="(color, i) in colors"
+            style="display: flex;gap: 16px">
+
+            <div :class="{container: true, [colors[i+1]]: true, grid: true}">
+                <div 
+                  :class="{'mh-auto': true, s1: true,e2: true,dondurma: true, [color]: true}" 
+                  :style="`animation-delay: -${Math.floor((i+1)*2, 3)/10}s`" 
+                />
+
+                <Code 
+                  class="mv-auto s2 e5"
+                  :code='`class="dondurma ${color}" style="animation-delay: -${Math.floor((i+1)*2, 3)/10}s"`' 
+                />
+            </div>
         </div>
     </div>
 
-    <div class="container white">
+    <!--<div class="container white">
         まわるドーナツ
         <div class="rotate" style="width: fit-content;">
         🍩
         </div>
-    </div>
+    </div>-->
 </template>
+
+<style scoped>
+.mv-auto{
+    margin-top: auto;    
+    margin-bottom: auto;
+}
+
+.mh-auto{
+    width: fit-content;
+    margin-left: auto; 
+    margin-right: auto;
+}
+</style>
+
+<script setup>
+const colors=ref(["white", "violet", "pink", "red", "orange", "yellow", "green", "blue"])
+</script>
