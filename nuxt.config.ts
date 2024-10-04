28a2b428d07c3c090dde748bd50e4c9e18c5a287
Rie Amasato <rie_amasato@icloud.com>
2024-10-04 00:35:17 +0000

    SEO用のDescriptionを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,20 @@ export default defineNuxtConfig({
   target: "static",
   css: ["@/assets/css/general.css", "@/assets/css/amasugar/amasugar.css"],
   app: {
-    buildAssetsDir: '/sugar/'
+    buildAssetsDir: '/sugar/',
+    head: {
+      title: "あまさとう パステル味のCSS集",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "device-width,initial-scale=1"
+        },
+        { 
+          name: "description",
+          content: "お砂糖をまとった（糖衣構文）ようなCSSを作りたいから始まったプロジェクト。パステル基調なCSSを目指して鋭意作成中。"
+        },
+      ]
+    }
   }
 })
