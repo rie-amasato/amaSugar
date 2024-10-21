f39d87548f86b9902f74e2c23a15538aff3f2924
Rie Amasato <rie_amasato@icloud.com>
2024-10-21 12:40:50 +0000

    チョコプレートのinput要素

---
+++ pages/ils-food-cake/input.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container green">
+        <div class="pd-4 fcg-4">
+            <input 
+                class="plate"
+                value="Happy birthday的な"
+            >
+            <input 
+                class="plate bitter"
+                value="biterチョコのプレートも"
+            >
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.fcg-4{
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+</style>
